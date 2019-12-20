2f7d1fb8275ac583bd02c46ef51eb98e168ec9d8
Kennedy Kori <kennedykori47@gmail.com>
2019-12-20 19:52:51 +0300

    Clean up CellGroup and make it extend Iterator and Spliterator

--- src/main/java/com/kori_47/sudoku/CellGroup.java
+++ src/main/java/com/kori_47/sudoku/CellGroup.java
@@ -3,33 +3,55 @@
  */
 package com.kori_47.sudoku;
 
-import static java.util.Objects.requireNonNull;
-
 import java.util.Map;
+import java.util.Optional;
+import java.util.Spliterator;
 
 /**
- * A {@code CellGroup} is a logical ordering of a group of cells in a LatinSquare. Examples of {@code CellGroup}s 
- * are Rows and Columns.
+ * This represents a logical ordering of a group of cells in a {@link LatinSquare}. Each {@code CellGroup}
+ * in a complete {@code LatinSquare} should contain each of the {@link Symbol symbols} in the {@code LatinSquare}
+ * exactly once. Examples of {@code CellGroup}s are Rows and Columns.
  * 
- * @author Kennedy Kori
+ * @author <a href="https://github.com/kennedykori">Kennedy Kori</a>
  *
  * @since Oct 17, 2019, 2:50:14 AM
  */
-public interface CellGroup<V> extends Formattable {
-	
-	void addCell(Cell<V> cell);
-	
-	Cell<V> removeCell(String cellId);
-	
-	default Cell<V> removeCell(Cell<V> cell) {
-		return removeCell(requireNonNull(cell, "cell cannot be null").id());
-	}
+public interface CellGroup<V> extends Formattable, Iterable<V>, Spliterator<V> {
 
-	Cell<V> getCell(String cellId);
+	/**
+	 * Returns an {@link Optional} describing a {@code Cell} with the given cell id, or an empty if no such 
+	 * {@code Cell} exists in this {@code CellGroup}.
+	 * 
+	 * @param cellId the id of the {@code Cell} to return.
+	 * 
+	 * @return an {@link Optional} describing a {@code Cell} with the given cell id, or an empty if no such 
+	 * {@code Cell} exists in this {@code CellGroup}.
+	 * 
+	 * @throws NullPointerException if {@code cellId} is {@code null}.
+	 */
+	Optional<Cell<V>> getCell(String cellId);
 	
-	int id();
+	/**
+	 * Returns a {@code String} that identifies this {@code CellGroup} in a given {@code LatinSquare}.
+	 * 
+	 * @return the unique identifier of this {@code CellGroup} in a {@code LatinSquare}.
+	 */
+	String id();
 	
+	/**
+	 * Returns the number of {@code Cell}s in this {@code CellGroup}. This should normally be equal
+	 * to the size of the {@code LatinSquare} in which this {@code CellGroup} belongs.
+	 * 
+	 * @return the number of {@code Cell}s in this {@code LatinSquare}.
+	 */
 	int size();
 	
+	/**
+	 * Returns a {@code Map} of the {@code Cell}s contained in this {@code CellGroup}. Modification of the 
+	 * returned {@code Map} should not alter the contents of this {@code CellGroup}. Implementations of this 
+	 * interface can also choose to return an unmodifiable {@code Map} instead to prevent modifications.
+	 * 
+	 * @return a {@code Map} of the {@code Cell}s contained in this {@code CellGroup}
+	 */
 	Map<String, Cell<V>> cells();
 }
