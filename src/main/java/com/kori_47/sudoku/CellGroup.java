b79614531242a950c156dab88e376676403fe173
Kennedy Kori <kennedykori47@gmail.com>
2019-12-21 22:32:27 +0300

    Add default implementations of CellGroup.getCell() methods

--- src/main/java/com/kori_47/sudoku/CellGroup.java
+++ src/main/java/com/kori_47/sudoku/CellGroup.java
@@ -3,6 +3,10 @@
  */
 package com.kori_47.sudoku;
 
+import static java.util.Objects.requireNonNull;
+
+import static com.kori_47.utils.ObjectUtils.requireInRange;
+
 import java.util.Map;
 import java.util.Optional;
 import java.util.Spliterator;
@@ -28,8 +32,51 @@ public interface CellGroup<V> extends Formattable, Iterable<V>, Spliterator<V> {
 	 * {@code Cell} exists in this {@code CellGroup}.
 	 * 
 	 * @throws NullPointerException if {@code cellId} is {@code null}.
+	 * 
+	 * @implSpec
+	 * The default implementation is equivalent to, for this {@code cellGroup}:
+	 * <pre> {@code
+	 * Cell<V> cell = cellGroup.cells().get(cellId);
+	 * return Optional.ofNullable(cell);
+	 * }
+	 * </pre>
 	 */
-	Optional<Cell<V>> getCell(String cellId);
+	default Optional<Cell<V>> getCell(String cellId) {
+		requireNonNull(cellId, "cellId cannot be null.");
+		return Optional.ofNullable(cells().get(cellId));
+	}
+
+	/**
+	 * Returns an {@link Optional} describing a {@code Cell} with the given x and y coordinates, or an 
+	 * empty if no such {@code Cell} exists in this {@code CellGroup}.
+	 * 
+	 * @param x the x coordinate of the {@code Cell} to return.
+	 * @param y the y coordinate of the {@code Cell} to return.
+	 * 
+	 * @return an {@code Optional} describing a {@code Cell} with the given x and y coordinates, or an
+	 * empty if no such {@code Cell} exists in this {@code CellGroup}.
+	 * 
+	 * @throws IllegalArgumentException if {@code x} and/or {@code y} is/are negative or greater than
+	 * {@link #size() size} of this {@code CellGroup}.
+	 * 
+	 * @implSpec
+	 * The default implementation is equivalent to, for this {@code cellGroup}:
+	 * <pre> {@code
+	 * return cellGroup.cells().values().stream()
+	 * 		.filter(c -> c.x() == x)
+	 * 		.filter(c -> c.y() == y)
+	 * 		.findFirst();
+	 * }
+	 * </pre>
+	 */
+	default Optional<Cell<V>> getCell(int x, int y) {
+		requireInRange(0, size(), x, "x must be non negative and less than " + size());
+		requireInRange(0, size(), y, "y must be non negative and less than " + size());
+		return cells().values().stream()
+				.filter(cell -> cell.x() == x)
+				.filter(cell -> cell.y() == y)
+				.findFirst();
+	}
 	
 	/**
 	 * Returns a {@code String} that identifies this {@code CellGroup} in a given {@code LatinSquare}.
