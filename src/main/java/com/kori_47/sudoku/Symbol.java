a114dd8548538fdaffce0c5c8baeaa076277e156
Kennedy Kori <kennedykori47@gmail.com>
2019-12-22 15:17:59 +0300

    Refactor Symbol to extend Comparable.
    Also Add a default compare to implementation
    
    Signed-off-by: Kennedy Kori <kennedykori47@gmail.com>

--- src/main/java/com/kori_47/sudoku/Symbol.java
+++ src/main/java/com/kori_47/sudoku/Symbol.java
@@ -16,7 +16,30 @@ package com.kori_47.sudoku;
  *
  * @since Oct 17, 2019, 12:15:56 AM
  */
-public interface Symbol<V> {
+public interface Symbol<V> extends Comparable<Symbol<V>> {
+	
+	/**
+	 * Performs order and equality comparisons on two {@link Symbol}s.
+	 * 
+	 * @param other another symbol to compare to this one.
+	 * 
+	 * @return the value {@code 0} if this {@code Symbol} is
+     *         equal to the argument {@code Symbol}; a value less than
+     *         {@code 0} if this {@code Symbol} is less than the argument
+     *         {@code Symbol}; and a value greater than {@code 0} if this
+     *         {@code Symbol} is greater than the argument {@code Symbol}.
+     * @implSpec
+     * The default implementation performs comparison based on the {@link #id() ids}
+     * of the two symbols. That is, the implementation is equivalent to:
+     * <pre> {@code
+	 * return Integer.compare(this.id(), other.id());
+	 * }
+	 * </pre>
+	 */
+	@Override
+	default int compareTo(Symbol<V> other) {
+		return Integer.compare(id(), other.id());
+	}
 	
 	/**
 	 * Returns the id of this symbol. 
