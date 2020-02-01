81500262d17cb2f2cac3e424fda161dfe9a18433
Kennedy Kori <kennedykori47@gmail.com>
2020-02-01 19:31:34 +0300

    Move SudokuVariant and SudokuVariants code into Sudoku interface

--- src/main/java/com/kori_47/sudoku/Sudoku.java
+++ src/main/java/com/kori_47/sudoku/Sudoku.java
@@ -3,6 +3,8 @@
  */
 package com.kori_47.sudoku;
 
+import static com.kori_47.utils.ObjectUtils.requireGreaterThanOrEqualTo;
+
 import java.util.Map;
 
 /**
@@ -113,4 +115,156 @@ public interface Sudoku<V> extends LatinSquare<V> {
 	 * @return a {@code Map} of the {@code Block}s contained in this {@code Sudoku}.
 	 */
 	Map<String, Block<V>> blocks();
+	
+	/**
+	 * This represent the properties needed to describe a valid {@link Sudoku}, i.e. size, the number of
+	 * {@link Row}s and {@link Column}s in a {@link Block} and the number of {@code Block}s in a 
+	 * {@code Sudoku}. This interface is used to describe a {@code Sudoku} with equal sized {@link Block blocks}
+	 * or regions. 
+	 * 
+	 * @author <a href="https://github.com/kennedykori">Kennedy Kori</a>
+	 *
+	 * @since Sat, 28 Dec 2019 11:30:27
+	 * 
+	 * @see Sudoku
+	 * @see SudokuVariants
+	 */
+	static interface SudokuVariant {
+
+		/**
+		 * Returns the size of the {@link Sudoku} described by this variant.
+		 * 
+		 * @return the size of the {@code Sudoku} described by this variant.
+		 */
+		int size();
+
+		/**
+		 * Returns the number of {@link Row}s in each {@link Block} inside the {@link Sudoku} described by
+		 * this variant.
+		 * 
+		 * @return the number of {@code Row}s in each {@code Block} inside the {@code Sudoku} described
+		 * by this variant.
+		 */
+		int blockRows();
+
+		/**
+		 * Returns the number of {@link Column}s in each {@link Block} inside the {@link Sudoku} described by
+		 * this variant.
+		 * 
+		 * @return the number of {@code Column}s in each {@code Block} inside the {@code Sudoku} described
+		 * by this variant.
+		 */
+		int blockColumns();
+
+		/**
+		 * Returns the number of horizontal {@link Block}s that make up the {@link Sudoku} described by this
+		 * variant. 
+		 * 
+		 * @return the number of horizontal {@code Block}s that make up the {@code Sudoku} described by this
+		 * variant. 
+		 */
+		int xBlocks();
+
+		/**
+		 * Returns the number of vertical {@link Block}s that make up the {@link Sudoku} described by this
+		 * variant. 
+		 * 
+		 * @return the number of vertical {@code Block}s that make up the {@code Sudoku} described by this
+		 * variant. 
+		 */
+		int yBlocks();
+	}
+	
+	/**
+	 * This represents a set of commonly used {@link SudokuVariant}s including the famous 9x9 variant.
+	 * 
+	 * @author <a href="https://github.com/kennedykori">Kennedy Kori</a>
+	 *
+	 * @since Sat, 28 Dec 2019 12:22:12
+	 */
+	static enum SudokuVariants implements SudokuVariant {
+
+		/**
+		 * Describes a <strong><i>4x4</i></strong> {@code Sudoku}.  
+		 */
+		VARIANT_4x4(4, 2, 2, 2, 2),
+		
+		/**
+		 * Describes a tall <strong><i>6x6</i></strong> {@code Sudoku}.  
+		 */
+		VARIANT_6x6_TALL(6, 2, 3, 2, 3),
+		
+		/**
+		 * Describes a fat <strong><i>6x6</i></strong> {@code Sudoku}.  
+		 */
+		VARIANT_6x6_FAT(6, 3, 2, 3, 2),
+		
+		/**
+		 * Describes a <strong><i>9x9</i></strong> {@code Sudoku}.  
+		 */
+		VARIANT_9x9(9, 3, 3, 3, 3),
+		
+		/**
+		 * Describes a <strong><i>16x16</i></strong> {@code Sudoku}.  
+		 */
+		VARIANT_16x16(16, 4, 4, 4, 4);
+		
+		/**
+		 * the size of the sudoku
+		 */
+		private final int size;
+		
+		/**
+		 * the number of rows in a block. 
+		 */
+		private final int blockRows;
+		
+		/**
+		 * the number of columns in a block.
+		 */
+		private final int blockColumns;
+		
+		/**
+		 * the number of horizontal blocks that make a complete sudoku.
+		 */
+		private final int xBlocks;
+		
+		/**
+		 * the number of vertical blocks that make a complete sudoku.
+		 */
+		private final int yBlocks;
+		
+		private SudokuVariants(int size, int blockRows, int blockColumns, int xBlocks, int yBlocks) {
+			this.size = requireGreaterThanOrEqualTo(1, size, "size must be greater tha or equal to 1.");
+			this.blockRows = requireGreaterThanOrEqualTo(1, blockRows, "blockRows must be greater tha or equal to 1.");
+			this.blockColumns = requireGreaterThanOrEqualTo(1, blockColumns, "blockColumns must be greater tha or equal to 1.");
+			this.xBlocks = requireGreaterThanOrEqualTo(1, xBlocks, "xBlocks must be greater tha or equal to 1.");
+			this.yBlocks = requireGreaterThanOrEqualTo(1, yBlocks, "yBlocks must be greater tha or equal to 1.");
+		}
+
+		@Override
+		public int size() {
+			return size;
+		}
+
+		@Override
+		public int blockRows() {
+			return blockRows;
+		}
+
+		@Override
+		public int blockColumns() {
+			return blockColumns;
+		}
+
+		@Override
+		public int xBlocks() {
+			return xBlocks;
+		}
+
+		@Override
+		public int yBlocks() {
+			return yBlocks;
+		}
+	}
 }
