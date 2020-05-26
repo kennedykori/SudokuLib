9e9af7b7c7d20f16608af0af3a1ba20a66f13126
Kennedy Kori <kennedykori47@gmail.com>
2020-05-26 21:03:29 +0300

    Add tests for default factory methods.

--- src/test/java/com/kori_47/sudoku/CellGroupsTest.java
+++ src/test/java/com/kori_47/sudoku/CellGroupsTest.java
@@ -157,7 +157,7 @@ public class CellGroupsTest {
 		assertNotNull(column);
 		assertNotNull(column1);
 
-		// assert that the returned rows were initialized correctly
+		// assert that the returned columns were initialized correctly
 		assertEquals(5, column.size());
 		assertEquals(5, column.cells().size());
 		assertEquals(2, column.x());
@@ -227,7 +227,7 @@ public class CellGroupsTest {
 	 * {@link CellGroups#boxBlockOf(String, int, java.util.Map, Cell, int, int)}.
 	 */
 	@Test
-	public void testboxBlockOf() {
+	public void testBoxBlockOf() {
 		// create test values
 		BoxBlock<Object> boxBlock = CellGroups.boxBlockOf(
 				"4", 4,
@@ -290,10 +290,10 @@ public class CellGroupsTest {
 	 * Test {@link CellGroups#boxBlockOf(String, int, java.util.Map, Cell, int, int)} throws the appropriate
 	 * exceptions incase of invalid parameters.
 	 * 
-	 * @see #testboxBlockOf1Exceptions()
+	 * @see #testBoxBlockOf1Exceptions()
 	 */
 	@Test
-	public void testboxBlockOfExceptions() {
+	public void testBoxBlockOfExceptions() {
 		// assert that an IllegalException is thrown where appropriate
 		assertThrows(IllegalArgumentException.class, () -> 
 			CellGroups.boxBlockOf(
@@ -395,6 +395,21 @@ public class CellGroupsTest {
 			)
 		); // assert that the endCell can be derived from the given properties
 
+		// assert that a SudokuException is thrown where appropriate
+		assertThrows(SudokuException.class, () ->
+			CellGroups.boxBlockOf(
+					"4", 3,
+					Arrays.asList(
+						Cells.of("2/2", 2, 2),
+						Cells.of("3/2", 3, 2),
+						Cells.of("3/3", 3, 3)
+					).stream().
+					collect(toMap(cell -> cell.id(), cell -> cell)),
+					Cells.of("2/2", 4, 4),
+					Cells.of("3/3", 3, 3)
+			)
+		); // assert that the product of the calculated blockRows and blockColumns is equal to the given size
+
 		// assert that a NullPointerException is thrown where appropriate
 		assertThrows(NullPointerException.class, () -> 
 			CellGroups.boxBlockOf(
@@ -430,10 +445,10 @@ public class CellGroupsTest {
 	 * Test {@link CellGroups#boxBlockOf(String, int, java.util.Map, Cell, Cell)} throws the appropriate
 	 * exceptions incase of invalid parameters.
 	 * 
-	 * @see #testboxBlockOfExceptions()
+	 * @see #testBoxBlockOfExceptions()
 	 */
 	@Test
-	public void testboxBlockOf1Exceptions() {
+	public void testBoxBlockOf1Exceptions() {
 		// assert that an IllegalException is thrown where appropriate
 		assertThrows(IllegalArgumentException.class, () -> 
 			CellGroups.boxBlockOf(
@@ -533,5 +548,168 @@ public class CellGroupsTest {
 			)
 		); // assert that we can't pass a null endCell
 		assertThrows(NullPointerException.class, () ->  CellGroups.boxBlockOf("2", 4, null, Cells.of("2/2", 2, 2), Cells.of("3/3", 3, 3))); // assert that we can't pass a null cells map
+	}
+
+	/**
+	 * Test {@link CellGroups#defaultRowFactory()} creates and returns the expected value.
+	 */
+	@Test
+	public void testDefaultRowFactory() {
+		// assert the the returned RowFactory isn't null
+		assertNotNull(CellGroups.defaultRowFactory());
+
+		// create test values
+		Row<Object> row = CellGroups.defaultRowFactory().createRow(
+				"1", 5,
+				Arrays.asList(
+					Cells.of("0/0", 0, 0),
+					Cells.of("1/0", 1, 0),
+					Cells.of("2/0", 2, 0),
+					Cells.of("3/0", 3, 0),
+					Cells.of("4/0", 4, 0)
+				).stream().collect(toMap(cell -> cell.id(), cell -> cell)), 
+			0);
+
+		Row<Object> row1 = CellGroups.defaultRowFactory().createRow(
+				"2", 3,
+				Arrays.asList(
+					Cells.of("0/1", 0, 1),
+					Cells.of("1/1", 1, 1),
+					Cells.of("2/1", 2, 1)
+				).stream().collect(toMap(cell -> cell.id(), cell -> cell)), 
+			1);
+
+		// assert the values created aren't null
+		assertNotNull(row);
+		assertNotNull(row1);
+
+		// assert that the returned rows were initialized correctly
+		assertEquals(5, row.size());
+		assertEquals(5, row.cells().size());
+		assertEquals(0, row.y());
+		assertEquals("1", row.id());
+		assertEquals(Cells.of("0/0", 0, 0), row.startCell());
+		assertEquals(Cells.of("4/0", 4, 0), row.endCell());
+
+		assertEquals(3, row1.size());
+		assertEquals(3, row1.cells().size());
+		assertEquals(1, row1.y());
+		assertEquals("2", row1.id());
+		assertEquals(Cells.of("0/1", 0, 1), row1.startCell());
+		assertEquals(Cells.of("2/1", 2, 1), row1.endCell()); 
+	}
+
+	/**
+	 * Test {@link CellGroups#defaultColumnFactory()} creates and returns the expected value.
+	 */
+	@Test
+	public void testDefaultColumnFactory() {
+		// assert the the returned ColumnFactory isn't null
+		assertNotNull(CellGroups.defaultColumnFactory());
+
+		// create test values
+		Column<Object> column = CellGroups.defaultColumnFactory().createColumn(
+				"9", 10,
+				Arrays.asList(
+					Cells.of("9/0", 9, 0),
+					Cells.of("9/1", 9, 1),
+					Cells.of("9/2", 9, 2),
+					Cells.of("9/3", 9, 3),
+					Cells.of("9/4", 9, 4),
+					Cells.of("9/5", 9, 5),
+					Cells.of("9/6", 9, 6),
+					Cells.of("9/7", 9, 7),
+					Cells.of("9/8", 9, 8),
+					Cells.of("9/9", 9, 9)
+				).stream().collect(toMap(cell -> cell.id(), cell -> cell)), 
+			9);
+
+		Column<Object> column1 = CellGroups.columnOf(
+				"1", 1,
+				Arrays.asList(
+					Cells.of("0/0", 0, 0)
+				).stream().collect(toMap(cell -> cell.id(), cell -> cell)), 
+			0);
+
+		// assert the values created aren't null
+		assertNotNull(column);
+		assertNotNull(column1);
+
+		// assert that the returned columns were initialized correctly
+		assertEquals(10, column.size());
+		assertEquals(10, column.cells().size());
+		assertEquals(9, column.x());
+		assertEquals("9", column.id());
+		assertEquals(Cells.of("9/0", 9, 0), column.startCell());
+		assertEquals(Cells.of("9/9", 9, 9), column.endCell());
+
+		assertEquals(1, column1.size());
+		assertEquals(1, column1.cells().size());
+		assertEquals(0, column1.x());
+		assertEquals("1", column1.id());
+		assertEquals(Cells.of("0/0", 0, 0), column1.startCell());
+		assertEquals(Cells.of("0/0", 0, 0), column1.endCell());
+	}
+
+	/**
+	 * Test {@link CellGroups#defaultBoxBlockFactory()} creates and returns the expected value.
+	 */
+	@Test
+	public void testDefaultBoxBlockFactory() {
+		// assert the the returned BoxBlockFactory isn't null
+		assertNotNull(CellGroups.defaultBoxBlockFactory());
+
+		BoxBlock<Object> boxBlock = (BoxBlock<Object>) CellGroups.defaultBoxBlockFactory().createBlock(
+			"4", 6,
+			Arrays.asList(
+				Cells.of("3/2", 3, 2),
+				Cells.of("4/2", 4, 2),
+				Cells.of("5/2", 5, 2),
+				Cells.of("3/3", 3, 3),
+				Cells.of("4/3", 4, 3),
+				Cells.of("5/3", 5, 3)
+			).stream().collect(toMap(cell -> cell.id(), cell -> cell)),
+			Cells.of("3/2", 3, 2),
+			Cells.of("5/3", 5, 3)
+		);
+
+		BoxBlock<Object> boxBlock1 = (BoxBlock<Object>) CellGroups.defaultBoxBlockFactory().createBlock(
+			"3", 10,
+			Arrays.asList(
+				Cells.of("0/2", 0, 2),
+				Cells.of("1/2", 1, 2),
+				Cells.of("2/2", 2, 2),
+				Cells.of("3/2", 3, 2),
+				Cells.of("4/2", 4, 2),
+				Cells.of("0/3", 0, 3),
+				Cells.of("1/3", 1, 3),
+				Cells.of("2/3", 2, 3),
+				Cells.of("3/3", 3, 3),
+				Cells.of("4/3", 4, 3)
+			).stream().collect(toMap(cell -> cell.id(), cell -> cell)),
+			Cells.of("0/2", 0, 2),
+			Cells.of("4/3", 4, 3)
+		);
+
+		// assert the value returned isn't null
+		assertNotNull(boxBlock);
+		assertNotNull(boxBlock1);
+	
+		// assert that the returned blocks were initialized correctly
+		assertEquals(6, boxBlock.size());
+		assertEquals(6, boxBlock.cells().size());
+		assertEquals(3, boxBlock.blockColumns());
+		assertEquals(2, boxBlock.blockRows());
+		assertEquals("4", boxBlock.id());
+		assertEquals(Cells.of("3/2", 3, 2), boxBlock.startCell());
+		assertEquals(Cells.of("5/3", 5, 3), boxBlock.endCell());
+	
+		assertEquals(10, boxBlock1.size());
+		assertEquals(10, boxBlock1.cells().size());
+		assertEquals(5, boxBlock1.blockColumns());
+		assertEquals(2, boxBlock1.blockRows());
+		assertEquals("3", boxBlock1.id());
+		assertEquals(Cells.of("0/2", 0, 2), boxBlock1.startCell());
+		assertEquals(Cells.of("4/3", 4, 3), boxBlock1.endCell());
 	}
 }
